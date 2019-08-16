d9c1d3d9f1a936433ddd152cfc09ddca028b09d4
lukasz.przytocki <lukasz.przytoci@aptitudesoftware.com>
2019-08-16 13:01:30 +0200

    Added data and pictures.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+insert into category (id, description, name)
+values(1, 'Elektronika dla każdego','Elektronika'),
+(2,'Meble do każdego pomieszczenia','Meble'),
+(3,'Artykuły gopodarstwa domowego','RTV_AGD'),
+(4,'Wyposażenie ogrodu','Ogród');
+
+insert into offer(id, description, img_url,price,title,category_id)
+values(1,'Super telewizor o przekątnej 55 cali','http://blabla2.jpg',1299,'Telewizor',1),
+(2,'Wypasione kino domowe firmy Sony, gra tak, że można robić festyn','http://blabla3.jpg',699,'Kino domowe',1),
+(3,'Idealny stół drewniany dla rodziny, 6 krzeseł gratis','http://blabla.jpg3',2699,'Stół drewniany',2);
